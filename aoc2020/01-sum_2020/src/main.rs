5a5ff8e3eec593b34fd5763172aa52717030affd
Kevin Gao <sudowork@gmail.com>
2020-12-03 00:10:11 -0800

    Implement advent day 1

---
+++ aoc2020/01-sum_2020/src/main.rs
@@ -0,0 +1,53 @@
+use std::collections::HashSet;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let input_file = env::args()
+        .skip(1)
+        .next()
+        .unwrap_or("./input.txt".to_string());
+    let input = read_lines(input_file);
+    let input: Vec<i32> = input.iter().map(|line| line.parse().unwrap()).collect();
+    let (a, b) = complements(&input, 2020).unwrap();
+    println!("Found Pair: ({}, {})", a, b);
+    println!("{}", a * b);
+
+    let (a, b, c) = triplets(&input, 2020).unwrap();
+    println!("Found Triplet: ({}, {}, {})", a, b, c);
+    println!("{}", a * b * c);
+}
+
+fn complements(xs: &Vec<i32>, sum: i32) -> Option<(i32, i32)> {
+    let xs: HashSet<_> = xs.iter().cloned().collect();
+    let complement = xs.iter().map(|x| sum - x).collect();
+    let pairs: Vec<_> = xs.intersection(&complement).cloned().collect();
+    match pairs.len() {
+        2 => Some((pairs[0], pairs[1])),
+        _ => None,
+    }
+}
+
+fn triplets(xs: &Vec<i32>, sum: i32) -> Option<(i32, i32, i32)> {
+    for x in xs.iter() {
+        match complements(xs, sum - x) {
+            Some((y, z)) => return Some((*x, y, z)),
+            _ => continue,
+        };
+    }
+    None
+}
+
+fn read_lines<P>(filepath: P) -> Vec<String>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filepath).unwrap();
+    let lines = io::BufReader::new(file).lines();
+    lines
+        .map(|l| String::from(l.unwrap().trim()))
+        .filter(|l| !l.is_empty())
+        .collect()
+}
