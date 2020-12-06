d11b72586579c1dc10699ec07882c59c464eac60
Kevin Gao <sudowork@gmail.com>
2020-12-05 23:33:12 -0800

    Implement day 6 part 2

--- aoc2020/06-customs/src/main.rs
+++ aoc2020/06-customs/src/main.rs
@@ -5,14 +5,20 @@ fn main() {
     let input_file = util::get_input_file("./input.txt");
     let groups = util::read_line_groups(input_file);
 
-    let count = get_total_declarations(&groups);
+    let count = sum_declarations(get_declarations, &groups);
     println!("Total declaration count: {}", count);
+
+    let count = sum_declarations(get_intersect_declarations, &groups);
+    println!("Total intersecting declaration count: {}", count);
 }
 
-fn get_total_declarations(groups: &Vec<Vec<String>>) -> usize {
+fn sum_declarations(
+    group_fn: fn(&Vec<String>) -> HashSet<char>,
+    groups: &Vec<Vec<String>>,
+) -> usize {
     groups
         .iter()
-        .map(|g| get_declarations(g))
+        .map(group_fn)
         .map(|ds| ds.len())
         .fold(0, |agg, ds| agg + ds)
 }
@@ -24,5 +30,19 @@ fn get_declarations(group: &Vec<String>) -> HashSet<char> {
         .fold(HashSet::new(), |mut set, c| {
             set.insert(c);
             set
+        })
+}
+
+fn get_intersect_declarations(group: &Vec<String>) -> HashSet<char> {
+    group
+        .iter()
+        .map(|line| {
+            line.chars().fold(HashSet::new(), |mut set, c| {
+                set.insert(c);
+                set
+            })
+        })
+        .fold(('a'..='z').collect(), |a, b| {
+            a.intersection(&b).cloned().collect()
         })
 }
