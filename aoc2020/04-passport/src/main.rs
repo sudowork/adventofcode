db0ca980a2e9df54c36cced82f7dcf890465b349
Kevin Gao <sudowork@gmail.com>
2020-12-06 16:35:44 -0800

    Rewrite problem 4 with decalarative rules

--- aoc2020/04-passport/src/main.rs
+++ aoc2020/04-passport/src/main.rs
@@ -1,8 +1,22 @@
 use regex::Regex;
 use std::collections::HashMap;
+use std::str::FromStr;
 use util;
 
+macro_rules! hashmap(
+    { $($key:expr => $value:expr),+ } => {
+        {
+            let mut m = ::std::collections::HashMap::new();
+            $(
+                m.insert($key, $value);
+            )+
+            m
+        }
+     };
+);
+
 type Passport = HashMap<String, String>;
+type ValidationRule = Box<dyn Fn(&str) -> bool>;
 
 const REQUIRED_FIELDS: [&str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 // const OPTIONAL_FIELDS: [&str; 1] = ["cid"];
@@ -49,64 +63,65 @@ fn is_valid(passport: &Passport) -> bool {
     if !has_required_fields(passport) {
         return false;
     }
-    if !validate_digits(passport.get("byr").unwrap(), 4, 1920, 2002) {
-        return false;
-    }
-    if !validate_digits(passport.get("iyr").unwrap(), 4, 2010, 2020) {
-        return false;
-    }
-    if !validate_digits(passport.get("eyr").unwrap(), 4, 2020, 2030) {
-        return false;
-    }
-    let hgt = passport.get("hgt").unwrap();
-    if !((validate_regex(hgt, &Regex::new(r"^\d+cm$").unwrap())
-        && validate_digits(&hgt[..3], 3, 150, 193))
-        || (validate_regex(hgt, &Regex::new(r"^\d+in$").unwrap())
-            && validate_digits(&hgt[..2], 2, 59, 76)))
-    {
-        return false;
-    }
-    if !validate_regex(
-        passport.get("hcl").unwrap(),
-        &Regex::new("^#[0-9a-f]{6}$").unwrap(),
-    ) {
-        return false;
-    }
-    if !validate_oneof(
-        passport.get("ecl").unwrap(),
-        ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].to_vec(),
-    ) {
-        return false;
-    }
-    if !validate_regex(
-        passport.get("pid").unwrap(),
-        &Regex::new(r"^\d{9}$").unwrap(),
-    ) {
-        return false;
-    }
-    true
-}
-
-fn validate_digits(digits: &str, len: usize, min: usize, max: usize) -> bool {
-    if digits.len() != len {
-        return false;
-    }
-    match digits.parse::<usize>() {
-        Ok(digits) => digits >= min && digits <= max,
-        Err(_) => false,
-    }
-}
-
-fn validate_regex(s: &str, re: &Regex) -> bool {
-    re.is_match(s)
-}
-
-fn validate_oneof(s: &str, options: Vec<&str>) -> bool {
-    options.iter().any(|&option| option == s)
+    let validation_rules = hashmap! {
+        "byr" => vec![is_len(4), is_parsable::<usize>(), is_bounded(1920, 2002)],
+        "iyr" => vec![is_len(4), is_parsable::<usize>(), is_bounded(2010, 2020)],
+        "eyr" => vec![is_len(4), is_parsable::<usize>(), is_bounded(2020, 2030)],
+        "hcl" => vec![matches_regex(Regex::new(r"^#[0-9a-f]{6}$").unwrap())],
+        "ecl" => vec![is_one_of(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].to_vec())],
+        "pid" => vec![matches_regex(Regex::new(r"^\d{9}$").unwrap())],
+        "hgt" => vec![Box::new(|hgt: &str|
+            (matches_regex(Regex::new(r"^\d+cm$").unwrap())(hgt)
+                && all(vec![is_parsable::<usize>(), is_bounded(150, 193)])(&hgt[..3]))
+            || (matches_regex(Regex::new(r"^\d+in$").unwrap())(hgt)
+                && all(vec![is_parsable::<usize>(), is_bounded(59, 76)])(&hgt[..2]))
+        )]
+    };
+    validate_passport(passport, &validation_rules)
 }
 
 fn has_required_fields(passport: &Passport) -> bool {
     REQUIRED_FIELDS
         .iter()
         .all(|f| passport.contains_key(&f.to_string()))
+}
+
+fn validate_passport(passport: &Passport, rules: &HashMap<&str, Vec<ValidationRule>>) -> bool {
+    for (&key, rules) in rules.iter() {
+        let value = passport.get(key).unwrap();
+        if !rules.iter().all(|rule| rule(value)) {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_len(len: usize) -> ValidationRule {
+    Box::new(move |val: &str| val.len() == len)
+}
+
+fn is_parsable<T>() -> ValidationRule
+where
+    T: FromStr,
+{
+    Box::new(|val: &str| val.parse::<T>().is_ok())
+}
+
+fn is_bounded(min: usize, max: usize) -> ValidationRule {
+    Box::new(move |val: &str| {
+        let val: usize = val.parse().unwrap();
+        val >= min && val <= max
+    })
+}
+
+fn matches_regex(re: regex::Regex) -> ValidationRule {
+    Box::new(move |val: &str| re.is_match(val))
+}
+
+fn is_one_of(options: Vec<&'static str>) -> ValidationRule {
+    Box::new(move |val: &str| options.iter().any(|&option| option == val))
+}
+
+fn all(rules: Vec<ValidationRule>) -> ValidationRule {
+    Box::new(move |val: &str| rules.iter().all(|rule| rule(val)))
 }
