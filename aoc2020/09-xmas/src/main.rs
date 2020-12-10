5a062052a431a59960aa7c68d9d2a694404717c8
Kevin Gao <sudowork@gmail.com>
2020-12-09 19:33:48 -0800

    Implement day 9, part 2

--- aoc2020/09-xmas/src/main.rs
+++ aoc2020/09-xmas/src/main.rs
@@ -6,7 +6,11 @@ fn main() {
 
     let sequence: Vec<u64> = input.iter().map(|l| l.parse().unwrap()).collect();
 
-    println!("First invalid: {}", find_first_invalid(&sequence, 25));
+    let invalid = find_first_invalid(&sequence, 25);
+    println!("First invalid: {}", invalid);
+
+    let encryption_weakness = find_encryption_weakness(invalid, &sequence);
+    println!("Encryption weakness: {:?}", encryption_weakness);
 }
 
 fn find_first_invalid(xs: &Vec<u64>, preamble: usize) -> u64 {
@@ -24,4 +28,22 @@ fn find_first_invalid(xs: &Vec<u64>, preamble: usize) -> u64 {
 fn sum2(sum: u64, xs: &HashSet<u64>) -> bool {
     let complements: HashSet<_> = xs.iter().filter(|&&x| x < sum).map(|x| sum - x).collect();
     xs.intersection(&complements).count() >= 2
+}
+
+fn find_encryption_weakness(sum: u64, xs: &Vec<u64>) -> Option<u64> {
+    // Using O(n^2) algo, but practically O(n) since num contiguous to form sum is likely a small number
+    for i in 0..xs.len() - 1 {
+        for j in (i + 1)..xs.len() {
+            let seq = &xs[i..j];
+            let cont_sum = seq.iter().fold(0, |a, b| a + b);
+            if cont_sum > sum {
+                break;
+            }
+            if cont_sum == sum {
+                println!("Seq: {:?}", seq);
+                return Some(seq.iter().min().unwrap() + seq.iter().max().unwrap());
+            }
+        }
+    }
+    None
 }
