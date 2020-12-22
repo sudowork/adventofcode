cd32fde3f43d3d427e31304fdb845558e767b8ce
Kevin Gao <sudowork@gmail.com>
2020-12-21 16:34:54 -0800

    Implement day 13, part 2

--- 2020/13-shuttle/src/main.rs
+++ 2020/13-shuttle/src/main.rs
@@ -14,6 +14,10 @@ fn main() {
         bus_time,
         (bus_time - time) * bus
     );
+
+    // part 2
+    let bus_time = find_bus_seq_time_iterative(&input);
+    println!("Bus sequence time: {}", bus_time);
 }
 
 fn parse_input(input: &Vec<String>) -> (usize, Vec<usize>) {
@@ -47,6 +51,45 @@ fn find_min_bus_cute(buses: &Vec<usize>, time: usize) -> (usize, usize) {
         }
     }
     (min_bus, min_time)
+}
+
+fn find_bus_seq_time_iterative(input: &Vec<String>) -> u64 {
+    let buses: Vec<(u64, u64)> = input[1]
+        .split(",")
+        .enumerate()
+        .filter(|(_, s)| s != &"x")
+        .map(|(i, s)| (i as u64, s.parse().unwrap()))
+        .collect();
+    let mut time = buses[0].1;
+    let mut multiple = buses[0].1;
+    for (offset, bus) in &buses[1..] {
+        while (time + *offset) % *bus != 0 {
+            time += multiple;
+        }
+        multiple *= *bus;
+    }
+    time
+}
+
+#[allow(dead_code)]
+fn find_bus_seq_time_brute_force(input: &Vec<String>) -> u64 {
+    let buses: Vec<(usize, usize)> = input[1]
+        .split(",")
+        .enumerate()
+        .filter(|(_, s)| s != &"x")
+        .map(|(i, s)| (i, s.parse().unwrap()))
+        .collect();
+    let (max_bus_offset, max_bus) = buses.iter().max_by(|x, y| x.1.cmp(&y.1)).unwrap();
+    (*max_bus as u64..)
+        .step_by(*max_bus)
+        .map(|time| time - *max_bus_offset as u64)
+        .skip_while(|&time| {
+            buses
+                .iter()
+                .any(|(offset, bus)| (time as u64 + *offset as u64) % *bus as u64 != 0)
+        })
+        .next()
+        .unwrap()
 }
 
 #[bench]
