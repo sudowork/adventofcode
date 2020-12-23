c4b98af6ef3213b98ac3e31bba0820c3743b4725
Kevin Gao <sudowork@gmail.com>
2020-12-23 15:14:37 -0800

    Clippy on 10

--- 2020/10-adapter/src/main.rs
+++ 2020/10-adapter/src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use util;
 
 fn main() {
     let input_file = util::get_input_file("./input.txt");
@@ -17,15 +16,15 @@ fn main() {
     println!("Num paths: {}", num_paths);
 }
 
-fn get_joltages(input: &Vec<String>) -> Vec<usize> {
+fn get_joltages(input: &[String]) -> Vec<usize> {
     let mut joltages: Vec<usize> = input.iter().map(|j| j.parse().unwrap()).collect();
-    joltages.sort();
+    joltages.sort_unstable();
     let device_joltage = joltages.iter().max().unwrap() + 3;
     joltages.push(device_joltage);
     joltages
 }
 
-fn count_joltage_diffs(joltages: &Vec<usize>) -> HashMap<usize, usize> {
+fn count_joltage_diffs(joltages: &[usize]) -> HashMap<usize, usize> {
     let mut diff_counts = HashMap::new();
     let diffs: Vec<usize> = joltages
         .iter()
@@ -41,7 +40,7 @@ fn count_joltage_diffs(joltages: &Vec<usize>) -> HashMap<usize, usize> {
     diff_counts
 }
 
-fn count_paths(orig_joltages: &Vec<usize>) -> usize {
+fn count_paths(orig_joltages: &[usize]) -> usize {
     let mut joltages: Vec<usize> = Vec::new();
     joltages.push(0);
     joltages.extend(orig_joltages);
@@ -52,6 +51,7 @@ fn count_paths(orig_joltages: &Vec<usize>) -> usize {
 
     for i in 1..num {
         let mut prev_sum = 0;
+        #[allow(clippy::needless_range_loop)]
         for j in (if i < 3 { 0 } else { i - 3 })..i {
             if lookup(i) - lookup(j) <= 3 {
                 prev_sum += memo[j];
